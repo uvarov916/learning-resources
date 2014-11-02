858518d1b24403c7b7103cb17c7c7267363ad12f
Ivan Uvarov <tonk2012@gmail.com>
2014-11-02 00:12:51 -0400

    Improved search, so it finds results even if words are in different order

--- js/app.js
+++ js/app.js
@@ -1,7 +1,25 @@
 // $(document).foundation();
 
+
 (function() {
-  var app = angular.module('ddRes', []);
+  var app = angular.module('ddRes', []).filter('search', function($filter){
+   return function(items, text){
+      if (!text || text.length === 0)
+        return items;
+
+      // split search text on space
+      var searchTerms = text.split(' ');
+
+      // search for single terms.
+      // this reduces the item list step by step
+      searchTerms.forEach(function(term) {
+        if (term && term.length)
+          items = $filter('filter')(items, term);
+      });
+
+      return items
+   };
+  });
 
   app.controller('ResourcesController', function() {
     this.generalInfo = generalInfo;
@@ -35,4 +53,5 @@
       description: "A weekly screencast covering Sketch app"
     }
   ];
+
 })();
