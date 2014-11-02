2eac280dcb9ec0ed711b7d5e763fd912c4fe9a87
Ivan Uvarov <tonk2012@gmail.com>
2014-11-01 22:27:05 -0400

    Added angular and moved the content from html to javascript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-$(document).foundation();
+// $(document).foundation();
+
+(function() {
+  var app = angular.module('ddRes', []);
+
+  app.controller('ResourcesController', function() {
+    this.items = resources;
+  });
+
+  var resources = [
+    {
+      name: "Sketch 3",
+      link: "http://bohemiancoding.com/sketch/",
+      description: "Sketch gives you the power, flexibility and speed you always wanted in a lightweight and easy-to-use package. Finally you can focus on what you do best: Design."
+    },
+    {
+      name: "SketchCasts",
+      link: "http://www.sketchcasts.net",
+      description: "A weekly screencast covering Sketch app"
+    }
+  ];
+})();
